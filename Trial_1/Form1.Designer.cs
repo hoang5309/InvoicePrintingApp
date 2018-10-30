42f3e5098d1148e5a51ca645e6ade1b67aecbff6
N03235893 <hoang_hoang5309@hotmail.com>
2018-10-30 14:26:47 -0400

    Last

--- Trial_1/Form1.Designer.cs
+++ Trial_1/Form1.Designer.cs
@@ -82,7 +82,7 @@
             // 
             // ExcBtn
             // 
-            this.ExcBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ExcBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ExcBtn.Location = new System.Drawing.Point(12, 100);
             this.ExcBtn.Name = "ExcBtn";
             this.ExcBtn.Size = new System.Drawing.Size(120, 53);
